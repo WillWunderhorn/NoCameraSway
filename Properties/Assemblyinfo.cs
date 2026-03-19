27da7f2cc42176d5c45a2cf1a5a893bfb66303f8
Wunderhorn <Narushev_pr1.19@st.ithub.ru>
2026-03-19 12:12:29 +0300

    Add project files.

---
+++ Properties/Assemblyinfo.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using NoCameraSway;
+using System.Reflection;
+
+[assembly: AssemblyTitle(BuildInfo.ModName)]
+[assembly: AssemblyCompany("Created by " + BuildInfo.ModAuthor)]
+
+[assembly: AssemblyVersion(BuildInfo.ModVersion)]
+[assembly: AssemblyFileVersion(BuildInfo.ModVersion)]
+
+[assembly: MelonInfo(typeof(NoCameraSway.NoCameraSway), BuildInfo.ModName, BuildInfo.ModVersion, BuildInfo.ModAuthor, BuildInfo.OriginalAuthor)]
+[assembly: MelonGame("Hinterland", "TheLongDark")]
+
+
+internal static class BuildInfo
+{
+    internal const string ModName = "NoCameraSway";
+    internal const string ModAuthor = "LittleWolfStorm";
+    internal const string OriginalAuthor = "WulfMarius";
+    internal const string ModVersion = "2.0.0";
+}
+
+
+
---
+++ src/NoCameraSway.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using MelonLoader;
+
+namespace NoCameraSway
+{
+    public class NoCameraSway : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            HarmonyInstance.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(Il2Cpp.vp_FPSCamera), nameof(Il2Cpp.vp_FPSCamera.Awake))]
+        private static class DisableCameraSway
+        {
+            internal static void Postfix(Il2Cpp.vp_FPSCamera __instance)
+            {
+                if (__instance == null)
+                {
+                    MelonLogger.Warning("vp_FPSCamera instance was null");
+                    return;
+                }
+
+                Il2Cpp.vp_FPSCamera.m_DisableAmbientSway = true;
+
+            }
+        }
+    }
+}
